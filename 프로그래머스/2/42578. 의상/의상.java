3b6bb848e485f82b55dc560789cad7c129dc949a
g2hyeong <72370234+g2hyeong@users.noreply.github.com>
2025-01-01 18:03:36 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 86.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,21 @@
+/**
+    
+*/
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        HashMap<String, Integer> hm = new HashMap<String, Integer>(clothes.length);
+        for (int i=0; i<clothes.length; i++) {
+            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        int answer = 1;
+        for (Map.Entry<String, Integer> entry : hm.entrySet()) {
+            answer = answer * (entry.getValue() + 1);
+        }
+        return answer - 1;
+    }
+}
